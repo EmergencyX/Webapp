e307641e8825a110c03bbe285b8e6598d80a349a
Raffael Stahl <raffael.stahl@gmail.com>
2016-03-04 00:57:52 +0000

    Install thumbnailer closes #1

--- config/app.php
+++ config/app.php
@@ -156,6 +156,8 @@ return [
         EmergencyExplorer\Providers\EventServiceProvider::class,
         EmergencyExplorer\Providers\RouteServiceProvider::class,
 
+        Folklore\Image\ImageServiceProvider::class,
+
     ],
 
     /*
@@ -201,6 +203,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        
+        'Thumbnail' => Folklore\Image\Facades\Image::class,
 
     ],
 
