3d57fa58db64222421d709393fb64ce4b88c52fa
Raffael Stahl <raffael.stahl@gmail.com>
2016-03-06 09:23:00 +0000

    Extend projects table (see #8)

---
+++ database/migrations/2016_03_06_091940_extend_projects_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ExtendProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->tinyInteger('status')->after('description');
+            $table->boolean('visible')->after('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropColumn(['status', 'visible']);
+        });
+    }
+}
