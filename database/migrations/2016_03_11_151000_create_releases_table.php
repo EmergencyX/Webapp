547dbc72aaaa9702f9214824598f1f7d55056f4b
Raffael Stahl <raffael.stahl@gmail.com>
2016-03-11 16:40:00 +0000

    New models and migrations

---
+++ database/migrations/2016_03_11_151000_create_releases_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReleasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('releases', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('project_repository_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('releases');
+    }
+}
---
+++ database/migrations/2016_03_11_151420_create_project_repositories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectRepositoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_repositories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('project_id')->unsigned();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('project_repositories');
+    }
+}
