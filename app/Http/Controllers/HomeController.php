c99532c1cae6404330ca5c8677f41dc305bf3b52
Raffael Stahl <raffael@mtb-news.de>
2016-03-29 06:39:53 +0200

    Use new thumbnailer

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,9 +6,13 @@ use Illuminate\Http\Request;
 
 use EmergencyExplorer\Http\Requests;
 
+use EmergencyExplorer\Project;
+
 class HomeController extends Controller
 {
     function index() {
-        return view('home.index');
+        $projects = Project::orderBy('updated_at','desc')->limit(4)->get();
+        
+        return view('home.index', compact('projects'));
     }
 }
--- app/Util/MediaUtil.php
+++ app/Util/MediaUtil.php
@@ -19,7 +19,7 @@ class MediaUtil {
             ->save(public_path('storage/'.$media->id.'-xs.jpg'));
         $imagine->thumbnail(new \Imagine\Image\Box(720,256), \Imagine\Image\ImageInterface::THUMBNAIL_OUTBOUND)
             ->save(public_path('storage/'.$media->id.'-md.jpg'));
-        $imagine->resize($imagine->getSize()->widen(1920))->save(public_path('storage/'.$media->id.'-md.jpg'));
+        $imagine->resize($imagine->getSize()->widen(1920))->save(public_path('storage/'.$media->id.'-lg.jpg'));
         logger()->info('Generated thumbnails for media', $media->toArray());
         
         //unlink($filePath); //Todo: Keep the original file for later re-processing?
@@ -27,7 +27,10 @@ class MediaUtil {
     }
   
   
-    public static function getThumbnail(Media $media) {
-        
+    public static function getThumbnail(Media $media = null, $size = 'xs') {
+        if ($media) {
+            return asset('storage/'.$media->id.'-'.$size.'.jpg');
+        }
+        return asset('storage/26-'.$size.'.jpg');
     }
 }
