471089c8f95f7241c0df123dff3ee8e1f6800266
Raffael Stahl <raffael.stahl@gmail.com>
2016-06-07 09:44:18 +0200

    Move favicons and include them

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/css/tether.min.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
     
-    
+    @include('layouts.favicon')
 </head>
 
 <body>
