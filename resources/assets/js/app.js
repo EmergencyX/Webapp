d9b5d6e643b2d1d3f12c3d14dfc1aa95f80fd4cb
noBlubb <raffael.stahl@gmail.com>
2016-09-21 17:32:23 +0000

    Update app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,10 +1,12 @@
-import Vue from 'vue';
-import ServerBrowser from './components/server-browser.vue';
+import Vue from 'vue'; //Framework für "Databinding" - am besten in server-browser.vue gucken
+import Centrifuge from 'centrifuge'; //Centrifugo Client - das ist ein Echtzeitserver, der die Updates verteilt
+import ServerBrowser from './components/server-browser.vue'; //Der eigentliche Serverbrowser
 
-import Centrifuge from 'centrifuge';
-
+//Erweiterung "Vue Debugger" in Chrome, immer ganz hilfreich
 Vue.config.devtools = true;
 
+//Neue Vue Instanz erstellen, Component deklarieren
+//window.data wird im entsprechenden View definiert (resources/views/multiplayer/browser.blade.php)
 let vm = new Vue(
     {
         el: 'body',
@@ -15,12 +17,14 @@ let vm = new Vue(
     }
 );
 
+//Initiale Daten vom Server abholen
+//Todo: Hier sollte lieber Centrifugo's "History" verwendet werden
 var request = new XMLHttpRequest();
-
 request.open("GET", "http://fms.emergencyx.de:8080");
 request.addEventListener('load', function(event) {
     if (request.status >= 200 && request.status < 300) {
         let sessions = JSON.parse(request.responseText);
+        //Event in Vue auslösen und neue Daten verteilen
         vm.$broadcast('masterserver_em4', sessions);
     } else {
         console.warn(request.statusText, request.responseText);
@@ -29,6 +33,7 @@ request.addEventListener('load', function(event) {
 request.send();
 
 
+//Verbindung zu Centrifugo aufnehmen, Updates abwarten (subscribe)
 let centrifuge = new Centrifuge(window.data.centrifugo);
 centrifuge.subscribe("masterserver_em4", function(message) {
     vm.$broadcast('masterserver_em4', message.data.sessions);
