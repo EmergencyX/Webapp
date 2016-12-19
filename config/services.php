71f631f6268003894aa9137a041011f05d0a4eba
Raffael Stahl <raffael@mtb-news.de>
2016-12-19 20:27:07 +0100

    config: fix namespace

--- config/services.php
+++ config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-        'model'  => EmergencyExplorer\User::class,
+        'model'  => EmergencyExplorer\Models\User::class,
         'key'    => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
