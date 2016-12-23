5d7a785bf24a282933ba0b8ac612259e6b8e3834
Raffael Stahl <raffael.stahl@gmail.com>
2016-12-23 16:05:02 +0100

    Fix typo in input field name

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -4,7 +4,7 @@
     <h1>Profil bearbeiten</h1>
 
     {!! Form::open(['action'=>['User\UserController@update', $user], 'method'=>'patch', 'files'=>true]) !!}
-    {!! Form::file('image') !!}
+    {!! Form::file('avatar') !!}
     {!! Form::submit('Hochladen', ['class'=>'btn btn-primary']) !!}
     {!! Form::close() !!}
 @endsection
