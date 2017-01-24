470a3905a9aa9e433d7b4e328488f97a3b541633
Raffael Stahl <raffael@mtb-news.de>
2017-01-24 14:04:40 +0100

    Reenable api auth; View for oauth callback

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -12,9 +12,7 @@ class ApiController extends Controller
      */
     public function getCaller()
     {
-        return User::find(1);
         return request()->user('api');
-
         /*abort_unless($user->can('show', $project), 401);
         abort_unless($user->tokenCan('show-project'), 401);
 
