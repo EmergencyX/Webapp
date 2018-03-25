ffa5e2d57c9c863e2d83d9764733060ea66b089e
Raffael Stahl <raffael.stahl@gmail.com>
2018-03-25 20:33:06 +0200

    feat: replace routes.php by routes/* files

---
+++ routes/api.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['prefix' => 'api', 'middleware' => ['bindings']], function () {
+    Route::get('mods/recent', 'Api\ProjectController@recent');
+    // Route::post('auth/login', 'Auth\AuthController@postLogin');
+
+    Route::get('users/{user}', 'Api\ProjectController@show');
+    Route::get('projects/{project}', 'Api\ProjectController@show');
+
+    Route::get('projects/{project}/releases', 'Api\ReleaseController@index');
+    Route::get('projects/{project}/releases/{release}', 'Api\ReleaseController@show');
+
+    Route::get('projects/{project}/images', 'Api\ImageController@index');
+    Route::get('projects/{project}/images/{image}', 'Api\ImageController@show');
+
+    Route::get('releases/download', 'Api\ReleaseController@download');
+
+
+    Route::group(['middleware' => 'auth:api'], function () {
+        //Route::get('projects/create', 'Api\ProjectController@create'); //Todo
+        //Route::post('projects', 'Api\ProjectController@store'); //Todo
+        //Route::patch('projects/{project}', 'Api\ProjectController@update'); //Todo
+        //Route::delete('projects/{project}', 'Api\ProjectController@delete'); //Todo
+
+        Route::post('projects/{project}/images/{image}', 'Api\ImageController@store');
+        Route::post('projects/{project}/images', 'Api\ImageController@store');
+        Route::delete('projects/{project}/images/{image}', 'Api\ImageController@remove');
+
+
+        Route::post('projects/{project}/releases', 'Api\ReleaseController@store');
+
+        Route::delete('projects/{project}/releases/{release}', 'Api\ReleaseController@remove');
+    });
+});
+
---
+++ routes/web.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', 'HomeController@index');
+Route::get('kontakt', 'HomeController@contact');
+
+Route::get('multiplayer/browser/{gameSlug}', 'Multiplayer\MultiplayerController@index');
+
+Route::get('mods', 'Project\ProjectController@index');
+Route::get('mods/{project}', 'Project\ProjectController@show')->where('project', '[0-9]+');
+Route::get('mods/{project}-{seo}', 'Project\ProjectController@show')->where(['project' => '[0-9]+', 'seo' => '.*']);
+
+Route::get('mods/create', 'Project\ProjectController@create'); //Todo
+Route::post('mods', 'Project\ProjectController@store'); //Todo
+Route::patch('mods/{project}', 'Project\ProjectController@update'); //Todo
+Route::delete('mods/{project}', 'Project\ProjectController@delete'); //Todo
+
+Route::get('mods/{project}/images', 'Project\ImageController@index');
+//Route::get('mods/{project}/images/{image}', 'Project\ImageController@show');
+
+Route::get('users/{user}', 'User\UserController@show')->where('user', '[0-9]+');
+Route::get('users/{user}-{seo}', 'User\UserController@show')->where(['user' => '[0-9]+', 'seo' => '.*']);
+Route::get('users/{user}/edit', 'User\UserController@edit');
+Route::patch('users/{user}', 'User\UserController@update');
+
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('mods/{project}/images/create', 'Project\ImageController@create');
+    Route::post('mods/{project}/images', 'Project\ImageController@store');
+    Route::get('mods/{project}/toggle-follow', 'Project\ProjectController@toggleFollow');
+    Route::get('mods/{project}/edit', 'Project\ProjectController@edit');
+
+    Route::get('mods/{project}/release/create', 'Project\ReleaseController@create');
+    Route::post('mods/{project}/releases', 'Project\ReleaseController@store');
+    Route::get('mods/{project}/releases/{release}', 'Project\ReleaseController@show');
+    Route::get('mods/{project}/releases', 'Project\ReleaseController@index');
+    Route::get('mods/{project}/releases/{release}/remove', 'Project\ReleaseController@remove');
+    Route::get('mods/{project}/releases/{toRelease}/download', 'Project\ReleaseController@download');
+
+    Route::get('mods/updatecheck', 'Project\ReleaseController@updateCheck');
+
+    Route::post('mods/{project}/images/{image}/remove', 'Project\ImageController@remove');
+});
+//END UPGRADED
+
+//Route::get('/download', 'HomeController@download');
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\LoginController@showLoginForm');
+Route::post('auth/login', 'Auth\LoginController@login');
+Route::get('auth/logout', 'Auth\LoginController@logout');
+Route::post('auth/logout', 'Auth\LoginController@logout');
