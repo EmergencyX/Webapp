8b6091c268a3e8530face40eddfc9dce09703d4b
Raffael Stahl <raffael.stahl@gmail.com>
2018-03-26 23:56:27 +0200

    chore: reenable api/mods/recent endpoint

--- routes/api.php
+++ routes/api.php
@@ -16,36 +16,35 @@ Route::group(['prefix' => 'hooks'], function () {
     Route::post('tus', 'TusController@hook');
 });
 
-Route::group(['prefix' => 'api', 'middleware' => ['bindings']], function () {
-    Route::get('mods/recent', 'Api\ProjectController@recent');
-    // Route::post('auth/login', 'Auth\AuthController@postLogin');
 
-    Route::get('users/{user}', 'Api\ProjectController@show');
-    Route::get('projects/{project}', 'Api\ProjectController@show');
+Route::get('mods/recent', 'Api\ProjectController@recent');
+// Route::post('auth/login', 'Auth\AuthController@postLogin');
 
-    Route::get('projects/{project}/releases', 'Api\ReleaseController@index');
-    Route::get('projects/{project}/releases/{release}', 'Api\ReleaseController@show');
+Route::get('users/{user}', 'Api\ProjectController@show');
+Route::get('projects/{project}', 'Api\ProjectController@show');
 
-    Route::get('projects/{project}/images', 'Api\ImageController@index');
-    Route::get('projects/{project}/images/{image}', 'Api\ImageController@show');
+Route::get('projects/{project}/releases', 'Api\ReleaseController@index');
+Route::get('projects/{project}/releases/{release}', 'Api\ReleaseController@show');
 
-    Route::get('releases/download', 'Api\ReleaseController@download');
+Route::get('projects/{project}/images', 'Api\ImageController@index');
+Route::get('projects/{project}/images/{image}', 'Api\ImageController@show');
 
-
-    Route::group(['middleware' => 'auth:api'], function () {
-        //Route::get('projects/create', 'Api\ProjectController@create'); //Todo
-        //Route::post('projects', 'Api\ProjectController@store'); //Todo
-        //Route::patch('projects/{project}', 'Api\ProjectController@update'); //Todo
-        //Route::delete('projects/{project}', 'Api\ProjectController@delete'); //Todo
-
-        Route::post('projects/{project}/images/{image}', 'Api\ImageController@store');
-        Route::post('projects/{project}/images', 'Api\ImageController@store');
-        Route::delete('projects/{project}/images/{image}', 'Api\ImageController@remove');
+Route::get('releases/download', 'Api\ReleaseController@download');
 
 
-        Route::post('projects/{project}/releases', 'Api\ReleaseController@store');
+Route::group(['middleware' => 'auth:api'], function () {
+    //Route::get('projects/create', 'Api\ProjectController@create'); //Todo
+    //Route::post('projects', 'Api\ProjectController@store'); //Todo
+    //Route::patch('projects/{project}', 'Api\ProjectController@update'); //Todo
+    //Route::delete('projects/{project}', 'Api\ProjectController@delete'); //Todo
 
-        Route::delete('projects/{project}/releases/{release}', 'Api\ReleaseController@remove');
-    });
+    Route::post('projects/{project}/images/{image}', 'Api\ImageController@store');
+    Route::post('projects/{project}/images', 'Api\ImageController@store');
+    Route::delete('projects/{project}/images/{image}', 'Api\ImageController@remove');
+
+
+    Route::post('projects/{project}/releases', 'Api\ReleaseController@store');
+
+    Route::delete('projects/{project}/releases/{release}', 'Api\ReleaseController@remove');
 });
 
